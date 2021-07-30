ecf0ce50cdaf33d15932fb35db4e2331df441e34
18yang <SubjuGateW@163.com>
2021-07-30 11:43:54 +0800

    网关配置‘

---
+++ gulimall-order/src/main/java/com/atguigu/gulimall/order/dao/PaymentInfoDao.java
@@ -0,0 +1,17 @@
+package com.atguigu.gulimall.order.dao;
+
+import com.atguigu.gulimall.order.entity.PaymentInfoEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 支付信息表
+ * 
+ * @author chenshun
+ * @email sunlightcs@gmail.com
+ * @date 2021-07-28 20:11:53
+ */
+@Mapper
+public interface PaymentInfoDao extends BaseMapper<PaymentInfoEntity> {
+	
+}
---
+++ gulimall-ware/src/main/java/com/atguigu/gulimall/ware/dao/WareSkuDao.java
@@ -0,0 +1,17 @@
+package com.atguigu.gulimall.ware.dao;
+
+import com.atguigu.gulimall.ware.entity.WareSkuEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 商品库存
+ * 
+ * @author chenshun
+ * @email sunlightcs@gmail.com
+ * @date 2021-07-28 20:16:33
+ */
+@Mapper
+public interface WareSkuDao extends BaseMapper<WareSkuEntity> {
+	
+}
